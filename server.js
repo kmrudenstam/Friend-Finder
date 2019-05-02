500f133225f71d26e19b254b1ae5a9c25ec30e7f
Karin Rudenstam <Karin@Karins-MacBook-Pro.local>
2019-05-02 12:57:31 -0700

    added server.js & apiRoute

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Sets up Express App
+var app = express();
+var PORT = 1000;
+
+// Sets up Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Add the application routes
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+
+// Access CSS files
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
